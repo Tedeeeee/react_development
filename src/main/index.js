ce397510cd125ddea04e639ceca0a430c6ae99c7
Tedeeee <gksrmf359@gmail.com>
2024-06-14 01:37:00 +0900

    react를 활용해서 웹 페이지 만들기

---
+++ src/main/index.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./index.css";
+import axios from "axios";
+
+function MainPageComponent() {
+  const [products, setProducts] = React.useState([]);
+  React.useEffect(function () {
+    axios
+      .get(
+        "https://2ef4932e-fd8b-4a66-a9f5-82871785a395.mock.pstmn.io/products"
+      )
+      .then(function (response) {
+        const products = response.data.products;
+        setProducts(products);
+      })
+      .catch(function (error) {
+        console.error("에러 발생 : ", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <div id="header">
+        <div id="header-area">
+          <img src="images/icons/logo.png" alt="" />
+        </div>
+      </div>
+      <div id="body">
+        <div id="banner">
+          <img src="images/banners/banner1.png" alt="" />
+        </div>
+        <h1>판매되는 상품들</h1>
+        <div id="product-list">
+          {products.map(function (product, index) {
+            return (
+              <div className="product-card">
+                <div>
+                  <img className="product-img" src={product.imageUrl} alt="" />
+                </div>
+                <div className="product-contents">
+                  <span className="product-name">{product.name}</span>
+                  <span className="product-price">{product.price}원</span>
+                  <span className="product-seller">
+                    <img
+                      className="product-avatar"
+                      src="images/icons/avatar.png"
+                      alt=""
+                    />
+                    <span>{product.seller}</span>
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div id="footer"></div>
+    </div>
+  );
+}
+
+export default MainPageComponent;
